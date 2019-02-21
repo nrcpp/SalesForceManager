970571fe435dadfa557d6ffb758a2848545ed985
Denis K <den.kasakov@gmail.com>
2019-02-21 14:30:15 +0200

    Boilerplate

---
+++ SalesForceManager/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesForceManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var api = new SalesForceAPI();
+            api.Connect();
+        }
+    }
+}
---
+++ SalesForceManager/SalesForceAPI.cs
@@ -0,0 +1,40 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesForceManager
+{
+    public class SalesForceAPI
+    {
+        public static void Log(string msg, [CallerMemberName] string caller = null) =>
+            Console.WriteLine($"[{caller}]: {msg}");
+        
+        public static bool Log(IRestResponse response, [CallerMemberName] string caller = null)
+        {            
+            try
+            {
+                if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    Log($"OK: {response.Content}", caller);
+                    return true;
+                }
+                else
+                    throw new Exception($"ERROR: {response.Content}");
+            }
+            catch (Exception ex)
+            {
+                Log(ex.Message, caller);
+                return false;
+            }
+        }
+
+
+        public void Connect()
+        {
+        }
+    }
+}
